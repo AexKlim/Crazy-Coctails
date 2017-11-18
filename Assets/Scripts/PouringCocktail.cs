fb2785927f8f4a7a511ab4d5e405497fac0f5a31
Klim <AexKlim@gmail.com>
2017-11-18 14:15:55 +0300

    Commit 1
    Added random generation of coctails and filling players glass

---
+++ Assets/Scripts/PouringCocktail.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PouringCocktail : MonoBehaviour {
+	public GameObject liquidPrefab;
+	public float pouringSpeed;
+	public Ingredient[] ingredients;
+
+	RandomCocktail currentSampleCocktail;
+	int currentIngredientNumber = 0;
+	GameObject liquid;
+	int size;
+
+	void Start () {
+		currentSampleCocktail = GameObject.FindObjectOfType<RandomCocktail> ();
+		size = currentSampleCocktail.ingredients.Length;
+		ingredients = new Ingredient[size];
+		for (int i = 0; i < size; i++) {															
+			ingredients [i] = new Ingredient ();
+		}
+		ingredients [0].startHeight = 0.04f;
+	}
+	void Update () {
+		if (currentIngredientNumber < size) {
+			if (Input.GetMouseButtonDown (0)) {
+				liquid = Instantiate (liquidPrefab, Vector3.zero, Quaternion.identity, transform);
+				liquid.transform.localPosition = new Vector3 (0, ingredients [currentIngredientNumber].startHeight, 0);
+				liquid.GetComponent<SpriteRenderer> ().color = currentSampleCocktail.ingredients [currentIngredientNumber].color;
+				ingredients [currentIngredientNumber].color = currentSampleCocktail.ingredients [currentIngredientNumber].color;
+			}
+			if (Input.GetMouseButton (0)) {
+				liquid.transform.localScale += new Vector3 (0, Time.deltaTime * pouringSpeed, 0);
+			}
+			if (Input.GetMouseButtonUp (0)) {
+				ingredients [currentIngredientNumber].amount = liquid.transform.localScale.y;
+				currentIngredientNumber++;
+				if (currentIngredientNumber < size) {
+					ingredients [currentIngredientNumber].startHeight = ingredients [currentIngredientNumber - 1].startHeight + ingredients [currentIngredientNumber - 1].amount * RandomCocktail.spriteMultiplier;
+				}
+			}
+		} else {
+			GameObject.Find ("GameManager").GetComponent<InGame> ().StartNewRound ();
+		}
+	}
+}
---
+++ Assets/Scripts/RandomCocktail.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCocktail : MonoBehaviour {
+	public GameObject liquidPrefab;
+	public int minIngredientsNumber;
+	public int maxIngredientsNumber;
+	public float minIngredientAmountPercent;
+	public Ingredient[] ingredients;
+	public static float spriteMultiplier;
+
+	void Awake(){
+		spriteMultiplier = liquidPrefab.GetComponent<SpriteRenderer> ().sprite.texture.height / liquidPrefab.GetComponent<SpriteRenderer> ().sprite.pixelsPerUnit;
+		int size = Random.Range (minIngredientsNumber, maxIngredientsNumber + 1);
+
+		ingredients = new Ingredient[size];
+		for (int i = 0; i < size; i++) {															
+			ingredients [i] = new Ingredient ();
+		}
+		float sum = 0;
+		for (int i = 0; i < size; i++) {
+			ingredients [i].amount = Random.Range(minIngredientAmountPercent, 1f);
+			sum += ingredients [i].amount;
+		}
+		for (int i = 0; i < size; i++) {
+			ingredients [i].amount /= sum;
+		}
+
+		float startHeight = 0;
+		for (int i = 0; i < size; i++) {
+			ingredients [i].startHeight = startHeight * spriteMultiplier + 0.04f;
+			ingredients [i].color = RandomColor ();
+			GameObject liquid = Instantiate (liquidPrefab, Vector3.zero, Quaternion.identity, transform);
+			liquid.transform.localPosition = new Vector3 (0, ingredients[i].startHeight, 0);
+			liquid.transform.localScale =  new Vector3 (1, ingredients[i].amount, 0.5f);
+			liquid.GetComponent<SpriteRenderer> ().color = ingredients[i].color;
+			startHeight += ingredients [i].amount;
+		}
+	}
+
+	Color RandomColor(){
+		float red = 0, green = 0, blue = 0;
+		int a = Random.Range (0, 3);
+		switch (a) {
+		case 0:
+			red = Random.value;
+			green = Random.Range(0.5f, 1f);
+			blue = Random.value;
+			break;
+		case 1:
+			red = Random.value;
+			green = Random.value;
+			blue = Random.Range(0.5f, 1f);
+			break;
+		case 2:
+			red = Random.Range(0.5f, 1f);
+			green = Random.value;
+			blue = Random.value;
+			break;
+		}
+		Color col = new Color(red, green, blue, 1f);
+		return col;
+	}
+}
