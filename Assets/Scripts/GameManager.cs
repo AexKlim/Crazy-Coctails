9497a4011207f56f4146113f79909d0b62e7867b
Klim <AexKlim@gmail.com>
2017-11-19 13:35:15 +0300

    Commit 2
    Добавлены GameManager, подсчет процентной разницы и автоматичесткая загрузка новых раундов

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+	public GameObject sampleCocktailPrefab;
+	public GameObject pouringCocktailPrefab;
+	public Transform sampleCocktailTransform;
+	public Transform pouringCocktailTransform;
+	public Text totalDifferenceText;
+	public Text mark;
+	public int perfectMark;
+	public int greatMark;
+	public int goodMark;
+	string newSampleCocktailName = "NewSampleCocktail";
+	string newPouringCocktailName = "NewOurCocktail";
+
+	RandomCocktail sampleCocktail;
+	PouringCocktail pouringCocktail;
+
+	void Start () {
+		StartNewRound ();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {
+			StartNewRound ();
+		}
+	}
+
+	public void StartNewRound(){
+		Destroy (GameObject.Find(newSampleCocktailName));
+		GameObject sampleCocktailGO = Instantiate (sampleCocktailPrefab, Vector3.zero, Quaternion.identity, sampleCocktailTransform);
+		sampleCocktailGO.transform.localPosition = Vector3.zero;
+		sampleCocktailGO.name = newSampleCocktailName;
+		sampleCocktail = sampleCocktailGO.GetComponent<RandomCocktail> ();
+		Destroy (GameObject.Find(newPouringCocktailName));
+		GameObject pouringCocktailGO = Instantiate (pouringCocktailPrefab, Vector3.zero, Quaternion.identity, pouringCocktailTransform);
+		pouringCocktailGO.transform.localPosition = Vector3.zero;
+		pouringCocktailGO.name = newPouringCocktailName;
+		pouringCocktail = pouringCocktailGO.GetComponent<PouringCocktail> ();
+	}
+
+	public IEnumerator CalculateSimilarity(){
+		float sum = 0;
+		for (int i = 0; i < sampleCocktail.ingredients.Length; i++) {
+			float difference = Mathf.Abs (sampleCocktail.ingredients [i].amount -  pouringCocktail.ingredients [i].amount) * 100;
+			sum += difference;
+		}
+		int intSum = Mathf.RoundToInt (sum);
+		totalDifferenceText.text = "TOTAL INGREDIENTS DIFFERENCE = " +intSum + " %";
+		totalDifferenceText.enabled = true;
+		if (intSum <= perfectMark)
+			mark.text = "PERFECT";
+		else if (intSum <= greatMark)
+			mark.text = "GREAT";
+		else if(intSum <= goodMark)
+			mark.text = "GOOD";
+		else 
+			mark.text = "BAD";
+		mark.enabled = true;
+
+		yield return new WaitForSeconds (1.5f);
+		totalDifferenceText.enabled = false;
+		mark.enabled = false;
+	}
+}
--- Assets/Scripts/PouringCocktail.cs
+++ Assets/Scripts/PouringCocktail.cs
@@ -11,9 +11,14 @@ public class PouringCocktail : MonoBehaviour {
 	int currentIngredientNumber = 0;
 	GameObject liquid;
 	int size;
+	GameManager gameManger;
+	float totalHeight;
+	bool similarityCalculated;
+	bool roundOver;
 
 	void Start () {
-		currentSampleCocktail = GameObject.FindObjectOfType<RandomCocktail> ();
+		gameManger = FindObjectOfType<GameManager> ();
+		currentSampleCocktail = FindObjectOfType<RandomCocktail> ();
 		size = currentSampleCocktail.ingredients.Length;
 		ingredients = new Ingredient[size];
 		for (int i = 0; i < size; i++) {															
@@ -22,25 +27,43 @@ public class PouringCocktail : MonoBehaviour {
 		ingredients [0].startHeight = 0.04f;
 	}
 	void Update () {
+		if (roundOver)
+			return;
 		if (currentIngredientNumber < size) {
 			if (Input.GetMouseButtonDown (0)) {
 				liquid = Instantiate (liquidPrefab, Vector3.zero, Quaternion.identity, transform);
 				liquid.transform.localPosition = new Vector3 (0, ingredients [currentIngredientNumber].startHeight, 0);
 				liquid.GetComponent<SpriteRenderer> ().color = currentSampleCocktail.ingredients [currentIngredientNumber].color;
-				ingredients [currentIngredientNumber].color = currentSampleCocktail.ingredients [currentIngredientNumber].color;
+				ingredients[currentIngredientNumber].color = currentSampleCocktail.ingredients [currentIngredientNumber].color;
 			}
-			if (Input.GetMouseButton (0)) {
-				liquid.transform.localScale += new Vector3 (0, Time.deltaTime * pouringSpeed, 0);
-			}
-			if (Input.GetMouseButtonUp (0)) {
-				ingredients [currentIngredientNumber].amount = liquid.transform.localScale.y;
-				currentIngredientNumber++;
-				if (currentIngredientNumber < size) {
-					ingredients [currentIngredientNumber].startHeight = ingredients [currentIngredientNumber - 1].startHeight + ingredients [currentIngredientNumber - 1].amount * RandomCocktail.spriteMultiplier;
+			if (liquid != null) {
+				if (Input.GetMouseButton (0)) {
+					liquid.transform.localScale += new Vector3 (0, Time.deltaTime * pouringSpeed, 0);
+					ingredients [currentIngredientNumber].amount = liquid.transform.localScale.y;
+					if (ingredients [currentIngredientNumber].amount * RandomCocktail.spriteMultiplier + ingredients [currentIngredientNumber].startHeight > 2f) {
+						liquid.transform.localScale = new Vector3 (1f, (2f - ingredients [currentIngredientNumber].startHeight) / RandomCocktail.spriteMultiplier, 1f);
+						ingredients [currentIngredientNumber].amount = liquid.transform.localScale.y;
+
+						gameManger.StartCoroutine ("CalculateSimilarity");
+						gameManger.Invoke ("StartNewRound", 2f);
+						similarityCalculated = true;
+						roundOver = true;
+					}	
+				}
+				if (Input.GetMouseButtonUp (0)) {
+					currentIngredientNumber++;
+					if (currentIngredientNumber < size) {
+						ingredients [currentIngredientNumber].startHeight = ingredients [currentIngredientNumber - 1].startHeight + ingredients [currentIngredientNumber - 1].amount * RandomCocktail.spriteMultiplier;
+					}
 				}
 			}
 		} else {
-			GameObject.Find ("GameManager").GetComponent<InGame> ().StartNewRound ();
+			if (!similarityCalculated) {
+				gameManger.StartCoroutine ("CalculateSimilarity");
+				gameManger.Invoke("StartNewRound", 2f);
+				similarityCalculated = true;
+				roundOver = true;
+			}
 		}
 	}
 }
